de80f2481ce4a53598dee02a8f91e235c818683f
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-07-29 14:44:13 +1000

    images done
    finished the scraping, now for the app

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,40 +1,27 @@
-from flask import Flask, render_template
-
-# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
-import pymongo
-
-# Create an instance of our Flask app.
-app = Flask(__name__)
-
-# Create connection variable
-conn = 'mongodb://localhost:27017'
-
-# Pass connection to the pymongo instance.
-client = pymongo.MongoClient(conn)
-
-# Connect to a database. Will create one if not already available.
-#  example db = client.team_db
-
-# Drops collection if available to remove duplicates
-#example  db.team.drop()
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 
 
+def scrape():
+    # browser = init_browser()
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
-# Create a root route / that will query your Mongo database and pass the mars data into an HTML template to display the data.
-@app.route('/')
-def index():
-    # Store the entire team collection in a list
-    # teams = list(db.team.find())
-    # print(teams)
+    listings = {}
 
-    # Return the template with the teams list passed in
-    return render_template('index.html', teams=teams)
+    url = "https://redplanetscience.com/"
+    browser.visit(url)
 
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
 
+    results = soup.find_all('div', class_='content_title')
+    news_title = soup.find('div', class_='content_title').get_text()
+    news = soup.find('div', class_='article_teaser_body').get_text()
+    
 
-# ------------------------------Store the return value in Mongo as a Python dictionary
+    # Quit the browser
+    browser.quit()
 
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return news
