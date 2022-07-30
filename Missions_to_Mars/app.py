f010d6ff618068dde715ce85dd524dc0203b227d
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-07-30 15:48:22 +1000

    Edited my code lots

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,7 +1,12 @@
-#example from lesson 3- q8 below as example
 
+import requests
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import time
 from flask import Flask, render_template
 import pymongo
+import scrape_mars
 
 app = Flask(__name__)
 
@@ -10,17 +15,33 @@ conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
 # connect to mongo db and collection
-db = client.store_inventory
-produce = db.produce
+db = client.mars_db
+collection = db.mars_data
 
 
 @app.route("/")
 def index():
     # write a statement that finds all the items in the db and sets it to a variable
-    inventory = produce.find()
+    inventory = collection.find()
 
     # render an index.html template and pass it the data you retrieved from the database
-    return render_template("index.html", inventory=inventory)
+    return render_template("index.html", collection =collection)
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    client.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
 
 
 if __name__ == "__main__":
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,27 +1,103 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
+from splinter import Browser
+import pandas as pd
+import requests
+from selenium import webdriver
+import pandas as pd
+import splinter
 
 
-def scrape():
+
+def Mars_news():
     # browser = init_browser()
+    # splinter method
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
-
-    listings = {}
-
-    url = "https://redplanetscience.com/"
-    browser.visit(url)
-
-    html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
-
-    results = soup.find_all('div', class_='content_title')
-    news_title = soup.find('div', class_='content_title').get_text()
-    news = soup.find('div', class_='article_teaser_body').get_text()
     
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    parent_class = soup.select_one('div', class_='list_text')
+    news_title = parent_class.find('div', class_='content_title').get_text()
+    news = parent_class.find('div', class_='article_teaser_body').get_text()
+    news_data = {
+        "content title": news_title,
+        "page article": news,
+    }
 
     # Quit the browser
     browser.quit()
+    return news_data
 
-    return news
+def mars_image():
+
+    # splinter method
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    # ------constaint errors throught the notebook without the above method added each time scraping happends on a new page-----
+
+    mars_url = 'https://spaceimages-mars.com/'
+    browser.visit(mars_url)
+
+    html_mars = browser.html
+
+    soup = BeautifulSoup(html_mars, 'html.parser')
+
+    featured_url = soup.find("img", class_="headerimage fade-in")["src"]
+    featured_image_url = f'https://spaceimages-mars.com/{featured_url}'
+    
+    return featured_image_url
+
+def Marsfacts():
+    url ='https://galaxyfacts-mars.com/'
+    tables = pd.read_html(url)
+    df = tables[0]
+    df.columns =["Comparison","Mars", "Earth"]
+    cleaned_df = df.iloc[1:, :]
+    html_table = cleaned_df.to_html()
+    
+    return html_table
+def mars_hemispheres():
+    
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    url_hem = 'https://marshemispheres.com/'
+    browser.visit(url_hem)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # Code to retrieve the image urls and titles for each hemisphere.
+
+    # First, we need to get a get a list of all of the hemispheres
+    links = browser.find_by_css('a.product-item img')
+
+    # Next, loop through those links, click the link, find the sample anchor, and return the href we want
+    for i in range(len(links)):
+        hemisphere = {}
+        
+        # We have to find the elements on each loop to avoid a stale element exception
+        browser.find_by_css('a.product-item img')[i].click()
+        
+        #  Sample image anchor tag and extract the href
+        sample_elem = browser.links.find_by_text('Sample').first
+        hemisphere['img_url'] = sample_elem['href']
+        
+        # Get Hemisphere title
+        hemisphere['title'] = browser.find_by_css('h2.title').text
+        
+        # Append hemisphere object to list
+        hemisphere_image_urls.append(hemisphere)
+        
+        # Finally, we navigate backwards
+        browser.back()
+    return hemisphere_image_urls
+
